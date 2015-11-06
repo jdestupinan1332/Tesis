1e2ee7b4c2341393216d70bf07540c413df6c267
Juan Estupiñán M <jd.estupinan1332@uniandes.edu.co>
2015-11-06 12:57:58 -0500

    Mods a autofit para que coja mejor los picos

--- Data/autofit.R
+++ Data/autofit.R
@@ -16,29 +16,41 @@ ch2 <- dat$V3
 ch3 <- dat$V4
 ch4 <- dat$V5
 
-test <- ch3[(((interval-1)*hours*60) + 1):(interval*hours*60)] 
+ch1M <- dat$V2
+ch2M <- dat$V3
+ch3M <- dat$V4
+ch4M <- dat$V5
+
+
+test <- ch3M[(((interval-1)*hours*12) + 1):(interval*hours*12)] 
 a <- 0
 b <- 0
 i <- 1
 lista <- 1
-for (i in 1:(hours*60 - 1)){
+for (i in 1:(hours*12 - 1)){
 	a <- test[i]
 	b <- test[i+1]
 	if  ((a-b) > diff) {lista <- c(lista, i)}
 }
+lista <- c(lista, hours*12)
 
 allfits <- rep(0,length(lista)-1)
-
+par(mfrow=c(length(allfits),1))
 for (i in 1:(length(lista)-1)){
 	ini <- lista[i]
 	fin <- lista[i+1]
 	ydata <- log2(test[ini:fin])
-	xdata <- seq(0,length(ydata), length.out=length(ydata))
+	xdata <- seq(0,length(ydata)*5, length.out=length(ydata))
 	fit <- lm(ydata~xdata)
 	allfits[i] <- 1/(summary(fit)$coefficients[2,1])
+	plot(xdata, ydata)
+	abline(fit)
 }
 
-print(lista/60)
 print(allfits)
 print(mean(allfits))
 	
+
+
+means <- c(means, mean(allfits))
+std <- c(std, sd(allfits))
